97279c8f2a7ee3ecf69c13bf3791b30de8f58e1a
bernd-hartmann <bernd.hartmann86@t-online.de>
2016-10-12 10:59:05 +0200

    Step 3 - Master Detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,90 @@
-
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "./hero"
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'// relative to root
+    templateUrl: 'app/app.component.html',
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]// relative to root
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
     public title: string = "my employees";
-    public hero:Hero = {id:1, name: "Mr. Windstorm"}
-    public ngOnInit (){
+    public hero: Hero = {id: 1, name: "Mr. Windstorm"}
+    public heroes: Array<Hero> = HEROES;
+
+    public selectedHero:Hero;
+
+    public onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    public ngOnInit() {
 
         console.log("app component init");
     }
-    public sayHello(){
+
+    public sayHello() {
         alert('hello');
     }
-
 }
+
+const HEROES: Array<Hero> = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magneta'},
+    {id: 16, name: 'RubberMan'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'Dr IQ'},
+    {id: 19, name: 'Magma'},
+    {id: 20, name: 'Tornado'}
+];
